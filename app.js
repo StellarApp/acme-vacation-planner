6f77d366da00891d280079750483184ff8e387e5
Stella Kim <testcodesk@gmail.com>
2019-08-25 16:30:20 -0400

    2nd attempt

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello from node");
-const foo = "bar";
-
-// setTimeout(() => console.log(foo), 2000);
-
-const interval = setInterval(() => console.log("interval"), 1000);
-
-setTimeout(() => clearInterval(interval), 5000);
